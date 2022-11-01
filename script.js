9aab5c446215c1ab91517d6931bce982bd962028
Beka Bantasdze <thelataito@gmail.com>
2022-11-01 16:25:43 +0400

    final

---
+++ script.js
@@ -0,0 +1,110 @@
+// let array = [5, 25, 89, 120, 36];
+
+// array.push('javascript','python')
+
+// console.log(array);
+
+// let array = [5, 25, 89, 120, 36];
+
+// array.unshift('html','css')
+// console.log(array);
+
+// let array = [5, 25, 89, 120, 36];
+
+// let newArray = array.length;
+// console.log(newArray);
+
+// let array = [5, 25, 89, 120, 36];
+
+// array.shift();
+// array.pop()
+// console.log(array);
+
+
+// დავალება 2 
+// let array = [ "ფორთოხალი","ბანანი","მსხალი" ];
+// let newArray = array.length;
+// console.log(newArray);
+
+// let array = [ "ფორთოხალი","ბანანი","მსხალი" ];
+// array.push("ვაშლი","ანანასი");
+// console.log(array);
+
+// let array = [ "ფორთოხალი","ბანანი","მსხალი" ];
+// array.unshift('საზამთრო')
+
+// console.log(array);
+
+// let array = [ "ფორთოხალი","ბანანი","მსხალი" ];
+// array.push("ვაშლი","ანანასი");
+// array.unshift('საზამთრო');
+// let newArray = array.length;
+// console.log(newArray);
+// array.splice(3,1,"მანგო");
+// console.log(array);
+// array.shift()
+// array.pop()
+// console.log(array);
+
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+
+// let newArray=array.map((item)=>item/3);
+
+// console.log(newArray);
+
+// დავალება 4 
+// let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+
+// let newArray =
+
+// დავალება 5
+// let languages = ['html', 'css', 'javascript','python', 'php'];
+
+// let newlanguage = languages.filter( (y) => y.length > 3 )
+// console.log(newlanguage);
+
+// დავალება 6
+
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+// let newWords=words.filter((item)=>item.includes('m') || item.includes('M'))
+// console.log(newWords);
+
+
+// დავალება 7
+
+// let arr1 = [1, 2];
+// let arr2 = [3, 4];
+// let arr3 = [5, 6];
+
+// let combined =[].concat(arr1,arr2,arr3);
+// console.log(combined);
+
+// დავლება 8
+
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+// arr.forEach(num => {
+//   sum += num;
+// });
+
+// console.log(sum);
+
+
+// let numbers = [-2, 5, 10, -5, -1, 1, 3, 4, -15];
+// let positiveNumbers = numbers.filter((item)=>item>0);
+
+// console.log(positiveNumbers);
+
+// let arr = [-1, -2, -3, 4];
+//  let newArr = arr.some((item)=>item>0);
+//  console.log(newArr);
+
+// let array =[23,45,32,5,87,7,3,98]
+// array.sort((a, b) => a - b);
+// //
+// console.log(array);
+// console.log(Math.min(...newArray))
+
+// let array =[1, 2, 3, 4, 5];
+// array.splice(3,0,'a','b','c',);
+// console.log(array);
